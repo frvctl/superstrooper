7d516b0fe3232f5f9d4f48b44291290251d8dd3c
polarcuke <polarcuke@gmail.com>
2013-02-18 13:30:19 -0500

    not really sure if i changed anything

--- app/controllers/test_controller.rb
+++ app/controllers/test_controller.rb
@@ -11,6 +11,6 @@ class TestController < ApplicationController
 		@participant = Participant.find(params[:id])
 		@data = ActiveSupport::JSON.decode(params[:test_data])
 		Participant.record_data(@participant, @data)
-		redirect_to survey_participant_path, :notice => "Data Submitted"
+		redirect_to survey_participant_path, :notice => "Test Completed! Your data has been submitted."
 	end
 end
